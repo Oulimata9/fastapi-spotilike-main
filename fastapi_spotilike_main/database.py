429d25081fec0e8f62d20f5d726836693d249514
Oulimata9 <85106954+Oulimata9@users.noreply.github.com>
2024-01-21 15:16:24 +0100

    m

--- fastapi_spotilike_main/database.py
+++ fastapi_spotilike_main/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "C:\projets\fastapi-spotilike-main\spotilike_db.sql"  # Remplacez par votre URL de base de données
+DATABASE_URL = "sqlite:///./spotilike_db.sql"  # Remplacez par votre URL de base de données
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
